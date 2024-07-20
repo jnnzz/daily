0bc7af2581f92f198f9a55b55268cded099792ac
Jan Lorenz <larocojanlorenz@gmail.com>
2024-07-20 19:33:52 +0800

    Add files via upload

---
+++ positive-neg.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+  int arr[] = {3, 4, 6, 7, -9, -3};
+  int i;
+  int last;
+
+  // while(i>=0){
+  //   last = arr[i] % 10;
+  //     if (last>=0)
+  //       printf("Positive numbers: %d", arr[i]);  
+  // } 
+  //   if (arr[i] > 0){
+  //     printf("%d", arr[i]);
+  //   }
+  // printf("\n");
+  
+  // while( i< 0){
+  //   last = arr[i] % 10;
+  //   if (last>=0)
+  //   printf("Negative numbers: %d", arr[i]);
+  // }
+  
+
+  return 0;
+}
+
+void posNeg(){
+  
+}
